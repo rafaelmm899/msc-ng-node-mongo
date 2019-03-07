db3b985ad499c19b1369e8b2ef68e7a7ab6d097b
Jose Mata <rafaelmm899@hotmail.com>
2019-03-06 23:08:45 -0400

    server configuration

--- api/index.js
+++ api/index.js
@@ -1,11 +1,15 @@
 'use strict'
 
 var mongoose = require('mongoose');
+var app = require('./app');
+var port = 3789;
 
 mongoose.connect('mongodb://127.0.0.1:27017/msc01',{useNewUrlParser: true},(error, response) => {
     if(error){
         throw error;
     }else{
-        console.log("Connection made successfully");
+        app.listen(port, ()=>{
+            console.log("local server is running");
+        })
     }
 });
