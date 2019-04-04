066179add799c73533a16a4e9e8e1700c2155973
Jose Mata <rafaelmm899@hotmail.com>
2019-04-03 22:58:04 -0400

    visualization of the artists and genres in the start component

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { Artist } from '../models/artist';
+import { ArtistService } from "../artist/artist.service";
+import { UserService } from "../user/user.service";
+import { GLOBAL } from 'src/global';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [ArtistService,UserService]
 })
 export class HomeComponent implements OnInit {
+	public artists: Artist[];
+	public token: string;
+	public nextPage :string;
+	public prePage : string;
+	public pageDefault: string;
+	public url: string;
 
-  constructor() { }
+  	constructor(
+		private _artistService: ArtistService,
+		private _userService: UserService
+	) { 
+		this.pageDefault = "1";
+		this.token = this._userService.getTokenInLocalStorage();
+		this.url = GLOBAL.url;
+	}
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+		this.getArtists(this.pageDefault);
+	}
+
+	getArtists(page:string){
+		this._artistService.getArtists(this.token,page).subscribe(
+			response => {
+				if(response.artists){
+					this.artists = response.artists.docs;
+				}
+			},
+			error => {
+				console.log(error);
+			}
+		)
+	}
 
 }
