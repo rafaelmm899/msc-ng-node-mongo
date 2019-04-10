9811394fc753b17a125457f7b560b2e9ede8bbb2
Jose Mata <rafaelmm899@hotmail.com>
2019-04-09 23:55:49 -0400

    player configuration

--- api/routes/song.js
+++ api/routes/song.js
@@ -11,7 +11,7 @@ var api = express.Router();
 api.post('/song',authenticated.ensureAuth,songController.save);
 api.get('/songs/:id', authenticated.ensureAuth,songController.getSongs);
 api.post('/upload_song/:id', [authenticated.ensureAuth, md_upload], songController.uploadFile);
-api.get('/get_song/:id', authenticated.ensureAuth, songController.getFile);
+api.get('/get_song/:file', songController.getFile);
 api.get('/song/:id',authenticated.ensureAuth,songController.getSong);
 api.put('/update_song/:id', authenticated.ensureAuth,songController.update);
 api.delete('/delete_song/:id', authenticated.ensureAuth,songController.deleteSong);
--- client/src/app/dashboard/dashboard.component.ts
+++ client/src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, DoCheck } from '@angular/core';
+import { Component, OnInit, DoCheck,ViewChild,Renderer2, ElementRef } from '@angular/core';
 import { User } from '../models/user';
 import { UserService } from '../user/user.service';
 import { GLOBAL } from 'src/global';
 import { Router,ActivatedRoute } from "@angular/router";
 
 import { Subscription } from "rxjs";
+import { Song } from '../models/song';
  
 
 @Component({
@@ -15,23 +16,28 @@ import { Subscription } from "rxjs";
 
 })
 export class DashboardComponent implements OnInit, DoCheck {
+	//@ViewChild('#player') playerElement: ElementRef;
 	public subscription: Subscription;
 	public items: string[];
 	public userLogged;
 	public url;
 	public profileImage;
 	public token: string;
+	public song: Song;
+	
 
 	constructor(
 		private _userService: UserService,
 		private _route: ActivatedRoute,
-		private _router: Router
+		private _router: Router,
+		private el: ElementRef,
+		private renderer: Renderer2
 	) {  
 		this.userLogged = _userService.getUserLogged();
 		this.token = _userService.getTokenInLocalStorage();
 		this.url = GLOBAL.url;
 		this.profileImage = 'assets/images/default-user-image.png'
-		
+		this.song = new Song("","",1,"","","","");
 	}
 
 	ngDoCheck(){
@@ -44,6 +50,7 @@ export class DashboardComponent implements OnInit, DoCheck {
 	}
 
 	ngOnInit() {
+		
 		this.checkImageUser();
 	}
 
@@ -70,14 +77,36 @@ export class DashboardComponent implements OnInit, DoCheck {
 		this._router.navigate(['']);
 	}
 
+	ngAfterViewInit() {
+		console.log(this.el.nativeElement.querySelector("audio"));
+  	}
+
+	updatePlayer(song: any){
+		let filePath = this.url+'get-song/'+song.file;
+		
+		this.renderer.setAttribute(this.el.nativeElement.querySelector("source"),"src",filePath);
+		this.el.nativeElement.querySelector("audio").load();
+		this.el.nativeElement.querySelector("audio").play();
+
+		//let reproductor = document.getElementById("source");
+		//(document.getElementById("player") as any).load();
+		//(document.getElementById("player") as any).play();
+
+		//document.getElementById("play-song-title").innerHTML = song.name;
+		//document.getElementById("play-song-artist").innerHTML = song.album.artist.name;
+	}
+
 	onActivate(componentReference) {
 		console.log(componentReference)
 		//componentReference.anyFunction();
 		//Below will subscribe to the searchItem emitter
 		if(componentReference.play){
 			componentReference.play.subscribe((song) => {
-			// Will receive the data from child here 
-			console.log(song);
+			
+				this.song = song;
+				this.updatePlayer(song);
+				//document.getElementById("play-image-album").setAttribute("src", imagePath);
+				console.log(song);
 			})
 		}
 	}
